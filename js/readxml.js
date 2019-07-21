ccdbce2dc21d26cbb04924feae228f29a55b79da
nyonmachi <51725741+nyonmachi@users.noreply.github.com>
2019-07-21 14:45:30 +0900

    Add files via upload

---
+++ js/readxml.js
@@ -0,0 +1,35 @@
+'use strict';
+{
+
+
+  function getData(){
+    const xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.open('GET','sample.xml');
+    xmlhttp.send();
+
+    xmlhttp.onreadystatechange = function(){
+      if( xmlhttp.readyStates !== 4){
+        return;
+      }else{
+        if( xmlhttp.status !== 200){
+          return;
+        }
+      }
+      //ここから通常時の処理
+      const output = document.getElementById('output');
+      let innerHTML;
+      const docelem = xmlhttp.responseXML.documentElement;
+      const name = docelem.getElementsByTagName('name');
+      for(let i=0 ; i<name.length ; i++){
+        output.innerHTML =  output.innerHTML + name[i].tagName + ':' + name[i].textContent + '<br>';
+      }
+    }
+  }
+
+  const btn = document.getElementById('btn');
+  btn.addEventListener('click',()=>{
+    getData();
+  });
+
+}
