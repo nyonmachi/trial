7427475f3fb6014e288574a80dd3e708682b6ca4
nyonmachi <51725741+nyonmachi@users.noreply.github.com>
2019-07-21 20:30:16 +0900

    Add files via upload

---
+++ js/readxml0.js
@@ -0,0 +1,36 @@
+'use strict';
+{
+
+//https://www.ipentec.com/document/javascript-xml-parsing
+  function getData(){
+    const xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function(){
+      if (xmlhttp.readyState === 4) {
+        if (xmlhttp.status === 200) {
+          const elem = document.getElementById("output");
+
+      //ここから通常時の処理
+
+      console.log('readyStates OK');
+      // const output = document.getElementById('output');
+      let innerHTML;
+      const docelem = xmlhttp.responseXML.documentElement;
+      const name = docelem.getElementsByTagName('name');
+      for(let i=0 ; i<name.length ; i++){
+        output.innerHTML =  output.innerHTML + name[i].tagName + ':' + name[i].textContent + '<br>';
+      }
+    }
+
+    xmlhttp.open('GET','sample.xml');
+    xmlhttp.send();
+  }
+}
+
+  const btn = document.getElementById('btn');
+  btn.addEventListener('click',()=>{
+    console.log('on');
+    // getData();
+  });
+
+}
