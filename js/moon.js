2155817c3b337d8f79ffd1598c42f476a0e972c7
nyonmachi <51725741+nyonmachi@users.noreply.github.com>
2019-07-21 21:04:59 +0900

    Add files via upload

--- js/moon.js
+++ js/moon.js
@@ -5,11 +5,23 @@
     xmlhttp.open('GET','moon.xml');
     xmlhttp.send();
 
+    xmlhttp.onreadystatechange = function(){
+      if(xmlhttp.readyState === 4){
+        if(xmlhttp.status === 200 ){
+
+
+
+        }// status
+      }else{
+        alert('readyState wrong')
+      }//xmlhttp.readyState
+    }// function() {
 
   }//getData()
 
   const btn  = document.getElementById('btn');
   btn.addEventListener('click',()=>{
+    console.log('moon 1');
     getData();
   });
 
